5a77bfa0389d44d3e31a72410e0384af9381ef54
Joao Lucas Caracciolo <caracciolojl@hotmail.com>
2024-05-09 16:31:23 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/caracciolo/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.caracciolo.workshopmongo.services;
+
+import com.caracciolo.workshopmongo.domain.User;
+import com.caracciolo.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
