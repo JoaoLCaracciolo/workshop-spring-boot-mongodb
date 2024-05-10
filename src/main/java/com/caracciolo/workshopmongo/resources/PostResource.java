372d99df051c5b77bad020802240af6555819a44
Joao Lucas Caracciolo <caracciolojl@hotmail.com>
2024-05-10 16:48:58 -0300

    Simple query with query methods

--- src/main/java/com/caracciolo/workshopmongo/resources/PostResource.java
+++ src/main/java/com/caracciolo/workshopmongo/resources/PostResource.java
@@ -2,10 +2,13 @@ package com.caracciolo.workshopmongo.resources;
 
 
 import com.caracciolo.workshopmongo.domain.Post;
+import com.caracciolo.workshopmongo.resources.util.URL;
 import com.caracciolo.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -19,6 +22,13 @@ public class PostResource {
     public ResponseEntity <Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
--- src/main/java/com/caracciolo/workshopmongo/services/PostService.java
+++ src/main/java/com/caracciolo/workshopmongo/services/PostService.java
@@ -1,10 +1,7 @@
 package com.caracciolo.workshopmongo.services;
 
 import com.caracciolo.workshopmongo.domain.Post;
-import com.caracciolo.workshopmongo.domain.User;
-import com.caracciolo.workshopmongo.dto.UserDTO;
 import com.caracciolo.workshopmongo.repository.PostRepository;
-import com.caracciolo.workshopmongo.repository.UserRepository;
 import com.caracciolo.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +18,9 @@ public class PostService {
     public Post findById(String id) {
         return repo.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 }
