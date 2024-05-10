beeb82b897b58fea31e87eef9a6b73f1f52d9e53
Joao Lucas Caracciolo <caracciolojl@hotmail.com>
2024-05-10 15:09:59 -0300

    Getting a user by id

--- src/main/java/com/caracciolo/workshopmongo/services/UserService.java
+++ src/main/java/com/caracciolo/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.caracciolo.workshopmongo.services;
 
 import com.caracciolo.workshopmongo.domain.User;
 import com.caracciolo.workshopmongo.repository.UserRepository;
+import com.caracciolo.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +17,10 @@ public class UserService {
     public List<User> findAll() {
         return repo.findAll();
     }
+
+    public User findById(String id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+    }
+
 }
